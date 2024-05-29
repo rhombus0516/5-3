02f4b79e999c3c6d5269702b236b01b0f23a4812
rhombus0516 <karaya0913@gmail.com>
2024-05-29 22:00:05 +0900

    [delete]

--- lesson05.php
+++ lesson05.php
@@ -1,18 +1,1 @@
 <?php
-
-// 手札
-$cards = [
-    ['suit'=>'heart', 'number'=>12],
-    ['suit'=>'joker', 'number'=>0],
-    ['suit'=>'spade', 'number'=>1],
-    ['suit'=>'diamond', 'number'=>2],
-    ['suit'=>'club', 'number'=>13],
-];
-
-function judge($cards) {
-    // この関数内に処理を記述
-}
-
-// 関数「judge」を呼び出して結果を表示する
-
-?>
