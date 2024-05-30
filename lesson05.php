29f1c1f2fa24aa93757539fa25e786c7e522a015
rhombus0516 <karaya0913@gmail.com>
2024-05-30 21:55:38 +0900

    [Add]バブルソート

--- lesson05.php
+++ lesson05.php
@@ -1,2 +1,21 @@
 <?php
     $arr = array(5, 2, 7, 9, 8, 1, 3, 6, 4);
+
+    function bubbleSort(&$arr) {
+        $n = count($arr);
+        for($i = 0; $i < $n - 1; $i++) {
+            for($j = 0; $j < $n - $i - 1; $j++) {
+                if($arr[$j] > $arr[$j + 1]) {
+                    $temp = $arr[$j];
+                    $arr[$j] = $arr[$j + 1];
+                    $arr[$j + 1] = $temp;
+                }
+            }
+        }
+    }
+
+    bubbleSort($arr);
+
+    foreach ($arr as $value) {
+        echo $value . " ";
+    }
