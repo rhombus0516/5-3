3cff99700fbe93abeae3f3804083f6588858676d
rhombus0516 <karaya0913@gmail.com>
2024-06-03 19:22:13 +0900

    [Add]最大公約数

--- lesson09.php
+++ lesson09.php
@@ -1,1 +1,10 @@
 ﻿<?php
+
+    function euclidean($x, $y) {
+        if ($y == 0) {
+            return $x;
+        }
+        return euclidean($y, (int)$x % $y);
+    }
+
+    var_dump(euclidean(12,18));
