c17e32e8038a3dd5cb35384c81589f8514b6eee1
rhombus0516 <karaya0913@gmail.com>
2024-06-03 19:36:30 +0900

    [Add]ハノイの塔

--- lesson10.php
+++ lesson10.php
@@ -1,1 +1,15 @@
 ﻿<?php
+
+    function hanoi($n, $from, $to, $aux) {
+        if ($n == 1) {
+            echo "$from から $to に移動\n";
+            return;
+        }
+        hanoi($n - 1, $from, $aux, $to);
+        echo "$from から $to に移動\n";
+        hanoi($n - 1, $aux, $to, $from);
+    }
+
+    $n = 3;
+
+    hanoi($n, 'A', 'C', 'B');
